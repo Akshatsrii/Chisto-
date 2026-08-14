5c6c6a717aefda42e4737f184aea6a4d61e794b5
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-08-14 14:56:59 +0530

    Link View Menu button to explore-menu section

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
           Choose from a diverse menu featuring a delectable array of dishes
           crafted with the finest ingredients and culinary expertise.
         </p>
-        <button>View Menu</button>
+        <a href="#explore-menu"><button>View Menu</button></a>
       </div>
     </div>
   )
